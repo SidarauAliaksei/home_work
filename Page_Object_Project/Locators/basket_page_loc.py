fd24168363e7aeebf80a34ba50f1cbd1538f5d57
Aliaksei Sidarau <www.bart@bk.ru>
2022-07-08 19:43:10 +0300

    added locators

--- Page_Object_Project/Locators/basket_page_loc.py
+++ Page_Object_Project/Locators/basket_page_loc.py
@@ -4,5 +4,9 @@ from selenium.webdriver.common.by import By
 class BasketPageLoc:
     basket_url_loc = 'http://automationpractice.com/index.php?controller=order'
 
+    basket_empty_loc = (By.XPATH, '//div[@class="shopping_cart"]//span[contains(text(), "(empty)")]')
+
     check_faded_short_sleeve_t_shirts_in_basket_loc = By.XPATH, "//a[text()='Faded Short Sleeve T-shirts']"
     check_blouse_in_basket_loc = (By.XPATH, "//p/a[text()='Blouse']")
+
+    basket_info_loc = (By.CLASS_NAME, 'navigation_page')
